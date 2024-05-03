64f5e12850c46fee3698eb5695e154d79b46ca4b
BenDavies1218 <148213274+BenDavies1218@users.noreply.github.com>
2024-05-04 09:38:38 +1000

    feat: class

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import Libary classes, book class, AudioBook class, Games class, Movie class, Music class
+
+const { Media } = require("./Media");
+
+// Create instances of classes
+// Lord of the Rings as an instance of book class
+
+let lotr = new Media("fellowship of the ring");
+
+console.log(lotr);
+
+// Do things with those instances
+
+// Create an instance of a libary
+// add media instances into the Library
+
+// Log the data in the Library
