55310d9fd938e3b10256de5bd1a219d0fbcdea8d
BenDavies1218 <14972@coderacademy.edu.au>
2024-05-05 14:14:51 +1000

    feat: render components

---
+++ pokemon-fetcher/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PokemonTeam from "./components/PokemonTeam";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon team Generator</h1>
+      <PokemonTeam />
+    </div>
+  );
+}
+
+export default App;
